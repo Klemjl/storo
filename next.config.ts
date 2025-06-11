afedde034bcadd85bb19634e9ab9b82a6b7cb5cc
Klemjl <juliaklemenchuck06@gmail.com>
2025-06-12 00:06:56 +0300

    add  header

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,19 @@
 import type { NextConfig } from "next";
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  style-src 'self';
+  img-src 'self' data: blob: https://cdn.pixabay.com https://img.freepik.com https://cloud.appwrite.io;
+  font-src 'self';
+  connect-src 'self' https://fra.cloud.appwrite.io/v1 wss://fra.cloud.appwrite.io/v1;
+  base-uri 'self';
+  form-action 'self';
+  object-src 'none';
+  frame-ancestors 'none';
+  upgrade-insecure-requests;
+`;
+
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
@@ -27,6 +41,19 @@ const nextConfig: NextConfig = {
         hostname: "cloud.appwrite.io",
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+          },
+        ],
+      },
+    ];
   },
 };
 
